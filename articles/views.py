79c7d266cf641f33ce38eeddef77b6856cc3b735
J Sivakumar <jsivakumar321@gmail.com>
2025-07-27 15:33:15 +0530

    Created urls views templates for CRUD of Articles

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from .models import Article
 # Create your views here.
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+    context_object_name = 'articles'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+    context_object_name = 'article'
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ('title', 'body')
+    template_name = 'article_edit.html'
+    context_object_name = 'article'
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    context_object_name = 'article'
+    success_url = reverse_lazy('article_list')
