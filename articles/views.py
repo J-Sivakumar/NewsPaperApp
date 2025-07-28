817dbcd6c55424069f75bedba9ce68a03f4550c2
J Sivakumar <jsivakumar321@gmail.com>
2025-07-28 19:06:58 +0530

    Set permissions, added authorization functionality

--- articles/views.py
+++ articles/views.py
@@ -1,33 +1,56 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from .models import Article
 # Create your views here.
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin,ListView):
     model = Article
     template_name = 'article_list.html'
     context_object_name = 'articles'
+    login_url = 'login'
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin,DetailView):
     model = Article
+    login_url = 'login'
     template_name = 'article_detail.html'
     context_object_name = 'article'
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin,UpdateView):
     model = Article
     fields = ('title', 'body')
+    login_url = 'login'
     template_name = 'article_edit.html'
     context_object_name = 'article'
 
-class ArticleDeleteView(DeleteView):
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+class ArticleDeleteView(LoginRequiredMixin,DeleteView):
     model = Article
     template_name = 'article_delete.html'
     context_object_name = 'article'
+    login_url = 'login'
     success_url = reverse_lazy('article_list')
 
-class ArticleCreateView(CreateView):
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+class ArticleCreateView(LoginRequiredMixin,CreateView):
     model = Article
-    fields = ('title', 'body', 'author')
+    fields = ('title', 'body',)
     template_name = 'article_new.html'
     # context_object_name = 'article'
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
